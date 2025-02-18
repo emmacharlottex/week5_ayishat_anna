59e96eef46365e890ce6675099bddbc08aa1e391
Anna Koutsaki <53444040+Ann3te@users.noreply.github.com>
2025-02-18 18:44:46 +0000

    EDIT: Modified the rock paper scissors code

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,14 +1,12 @@
 # imported random module for random.randint to be applied
 import random
-# created user variable to add input function for user to be able to enter their choice, added .lower() method to make it case insensitive
-user = input('Enter your choice of Rock, Paper or Scissors using R, P or S: ').lower()
-# defined the letters which will be recognised as rock, paper, scissors, created them in a list to be able to have them as separate items
 
+# defined the letters which will be recognised as rock, paper, scissors, created them in a list to be able to have them as separate items
 allowed_letters = ['r', 'p', 's']
 
 # defined function called user_choice with parameter 'tool'
 def user_choice(tool):
-# created an if statement to determine which output is shown to the user dependant on which tool they picked
+# created an if statement to determine which output is shown to the user dependent on which tool they picked
     if tool == 'r':
         print('You have chosen: Rock')
     if tool == 'p':
@@ -17,8 +15,6 @@ def user_choice(tool):
         print('You have chosen: Scissors')
     # returning the value back to the function to use again
     return tool
-# created another variable called userchoice to store the result of the function
-#userchoice = user_choice(user)
 
 # defined function called computer
 def computer():
@@ -34,33 +30,26 @@ def computer():
     elif number == 2:
         print("Computer's choice of tool: Scissors")
         return 's'
-# created computer_choice variable to store the result of the computer
-#computer_choice = computer()
 
 # created a dictionary of winning outcomes
 winning_cases = {"r":"s", "p":"r", "s":"p"}
-# created an if statement to state if both user and computer have the same outcome, it will be a draw
-# if userchoice == computer_choice:
-#     print('You have drawn! Try again')
-# # used elif statement that compares the key of the dictionary with the value of the computer choice which has been determined
-# #winning_cases["r"]
-# elif winning_cases[userchoice] == computer_choice:
-#     print('You have won!')
-# # if other conditions aren't met, this means the computer has won so else statement is inputted
-# else:
-#     print("The computer has won!")
-
 
 score = 0
 user_score = 0
 computer_score = 0
+
 while score < 5:
+    # created user variable to add input function for user to be able to enter their choice, added .lower() method to make it case insensitive
+    user = input('Enter your choice of Rock, Paper or Scissors using R, P or S: ').lower()
 # created a while loop to ensure correct letters are used
     while user not in allowed_letters:
         print('Invalid Response! Please use R, P, S to select your tool')
         # gave input again to allow the user to try again if they input wrong letter
         user = input('Enter your choice of Rock, Paper or Scissors using R, P or S: ').lower()
+
+    # created a variable called userchoice to store the result of the function
     userchoice = user_choice(user)
+    # created computer_choice variable to store the result of the computer
     computer_choice = computer()
     if userchoice == computer_choice:
         print('You have drawn! Try again')
@@ -77,8 +66,4 @@ while score < 5:
         print("The computer has won!")
     print(f'User Score: {user_score}')
     print(f'Computer Score: {computer_score}')
-    user = input('Enter your choice of Rock, Paper or Scissors using R, P or S: ').lower()
 
-
-print(f'User Score: {user_score}')
-print(f'Computer Score: {computer_score}')
